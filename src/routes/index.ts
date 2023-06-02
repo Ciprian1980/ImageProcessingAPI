c812f16f73c2b3a993d66d9cf5dc2595d1c6c195
Ciprian Popa <ciprian_popa2002@yahoo.com>
2023-06-02 10:53:31 -0400

    importing a picture

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+const sharp = require("sharp");
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.send('main api route');
+  res.send('Main api route');
 })
 
 routes.get('/images', (req, res) => {
-  res.send('Processing image')
+  res.send('Processing image');
+  const getMetadata = async (): Promise<any> => {
+    try {
+      const metadata = await sharp('../cd0292-building-a-server-project-starter/images/encenadaport.jpg').metadata();
+      console.log(metadata);
+    } catch (error) {
+      console.log(`An error occurred during processing: ${error}`);
+    }
+  }
+  getMetadata();
 })
 
 
