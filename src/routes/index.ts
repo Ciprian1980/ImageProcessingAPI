204fe893757a6921daddaf9754c7f61c71b82bcb
Ciprian Popa <ciprian_popa2002@yahoo.com>
2023-07-28 11:31:54 -0400

    working on image

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,18 +3,28 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require("sharp");
 const routes = express.Router();
+const app = express();
+const fileUpload = require('express-fileupload');
+const image = import('./api/fjord.jpg');
 
 routes.get('/', (req, res) => {
   res.send('Main api route');
 })
 
+// app.use(fileUpload());
+
+// app.post('/upload', (req, res) => {
+//   // We'll handle the image upload here
+//   console.log('image:', image)
+// });
+
 routes.get('/images', (req, res) => {
   res.send('Processing image');
   const getMetadata = async (): Promise<any> => {
     try {
-      const filePath = path.join(__dirname, './assets');
-      const metadata = await sharp(fs.openSync(filePath, 'fjord.jpeg')).metadata();
-      console.log(metadata);
+      const filePath = express.static(path.join(__dirname, image));//I could eventually erase it
+      const metadata = await sharp(fs.openSync(filePath)).metadata();
+      console.log('image path is:', '/assets/fjord.jpeg');
     } catch (error) {
       console.log(`An error occurred during processing: ${error}`);
     }
@@ -24,7 +34,8 @@ routes.get('/images', (req, res) => {
   async function resizeImage() {
     try {
       const filePath = path.join(__dirname, './assets');
-      const metadata = await sharp(fs.openSync(filePath, 'fjord.jpeg')).metadata();
+      const metadata = await sharp(fs.openSync('../assets/fjord.jpeg', 'r')).metadata();
+      console.log(metadata)
       await sharp(metadata)
         .resize({
           width: 150,
